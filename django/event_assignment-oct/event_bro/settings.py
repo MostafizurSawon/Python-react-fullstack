da4f4ce1f5c7eceff255291535de3441478c894f
Md Mostafizur Rahman <mostafizur1102@gmail.com>
2024-10-30 12:34:08 +0600

    requirements.txt added and readme file added

--- django/event_assignment-oct/event_bro/settings.py
+++ django/event_assignment-oct/event_bro/settings.py
@@ -16,6 +16,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -111,13 +112,19 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+# TIME_ZONE = 'UTC'
 
-USE_I18N = True
+# USE_I18N = True
 
+# USE_TZ = True
+
+TIME_ZONE = 'Asia/Dhaka'
 USE_TZ = True
+USE_L10N = True
+LANGUAGE_CODE = 'en-us' 
+
 
 
 # Static files (CSS, JavaScript, Images)
